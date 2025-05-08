2462f1346bbe6b90a5ecd5d5581a0575569bf507
ChananchidaJermthai <itthichai.faen@bumail.net>
2025-05-09 01:45:18 +0700

    Item Heal Update

---
+++ Assets/Scripts/Items/ItemHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ItemHealth : MonoBehaviour
+{
+    private PlayerController playerRef;
+
+    private void Start()
+    {
+        playerRef = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            playerRef.hp += 1;
+            Debug.Log("ผู้เล่นเข้าสู่โซน!");
+
+        }
+    }
+}
